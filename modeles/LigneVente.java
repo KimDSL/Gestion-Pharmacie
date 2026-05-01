2413faeb3a44cd7704582ecac992d0f4ecd35e40
Aboubakar-siddik <siddikissa622@gmail.com>
2026-05-02 00:12:50 +0200

    Modification du projet Java

--- modeles/LigneVente.java
+++ modeles/LigneVente.java
@@ -1,45 +1,26 @@
-import java.util.Scanner;
-
 public class LigneVente {
 
-    // Attributs privés
+    // Attributs
     private Medicament medicament;
-    private int quantite;
-    private double sousTotal;
+    private int        quantite;
+    private double     sousTotal;
 
     // Constructeur
-    public LigneVente(Medicament m, int qte) {
-        this.medicament = m;
-        this.quantite = qte;
-        this.sousTotal = m.getPrix() * qte;
+    public LigneVente(Medicament medicament, int quantite) {
+        this.medicament = medicament;
+        this.quantite   = quantite;
+        this.sousTotal  = medicament.getPrix() * quantite;
     }
 
     // Getters
-    public Medicament getMedicament() {
-        return medicament;
-    }
+    public Medicament getMedicament() { return medicament; }
+    public int        getQuantite()   { return quantite; }
+    public double     getSousTotal()  { return sousTotal; }
 
-    public int getQuantite() {
-        return quantite;
-    }
-
-    public double getSousTotal() {
-        return sousTotal;
-    }
-
-    // Modifier la quantité de la ligne de vente
-    public void modifierQuantite(int nouvelleQte) {
-        if (nouvelleQte > 0) {
-            this.quantite = nouvelleQte;
-            this.sousTotal = medicament.getPrix() * this.quantite;
-        } else {
-            System.out.println("La quantité doit être supérieure à 0.");
-        }
-    }
-    // Afficher les informations de la ligne de vente
-    public void afficherUneLigneAchat() {
-        System.out.println("Nom : " + medicament.getNomCommercial());
-        System.out.println("Quantité : " + quantite);
-        System.out.println("Sous-total : " + sousTotal + " FCFA");
+    // afficher les détails de la ligne de vente
+    public void afficher() {
+        System.out.println("  - " + medicament.getNomCommercial()
+                + " x" + quantite
+                + " → " + sousTotal + " FCFA");
     }
 }
